b67480505cc71371072a3790e3323980cbdfe832
jeroen <jeroen.vanschijndel@otoy.com>
2021-04-16 19:38:25 +0200

    -windoh

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <windows.h>
 #include "gl/gl.h"
 
+#include "GLFW/glfw3.h"
+
 void main()
 {
-	
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
 
+
+	while(1)
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
 }
