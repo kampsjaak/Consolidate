4ba192090b61a09b42ad0f950f09901d39052a5e
kampsjaak <rotor3d@gmail.com>
2021-04-16 18:51:10 +0200

    adsf

---
+++ Consolidate.cpp
@@ -0,0 +1,51 @@
+﻿// Consolidate.cpp : Defines the entry point for the application.
+//
+
+#include "Consolidate.h"
+#include "winuser.h"
+#include "winerror.h"
+
+
+using namespace std;
+//using namespace winuser;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+    window();
+	return 0;
+}
+
+void window()
+{
+    if (m_hInstance == NULL)
+        m_hInstance = (HINSTANCE)GetModuleHandle(NULL);
+
+    HICON hIcon = NULL;
+    WCHAR szExePath[MAX_PATH];
+    GetModuleFileName(NULL, szExePath, MAX_PATH);
+
+    // If the icon is NULL, then use the first one found in the exe
+    if (hIcon == NULL)
+        hIcon = ExtractIcon(m_hInstance, szExePath, 0);
+
+    // Register the windows class
+    WNDCLASS wndClass;
+    wndClass.style = CS_DBLCLKS;
+    wndClass.lpfnWndProc = MainClass::StaticWindowProc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = m_hInstance;
+    wndClass.hIcon = hIcon;
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndClass.lpszMenuName = NULL;
+    wndClass.lpszClassName = m_windowClassName.c_str();
+
+    if (!RegisterClass(&wndClass))
+    {
+        DWORD dwError = GetLastError();
+        if (dwError != ERROR_CLASS_ALREADY_EXISTS)
+            return HRESULT_FROM_WIN32(dwError);
+    }
+}
