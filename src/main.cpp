b6aa1378cafc88c0bb1b88132355967e2d28e96f
kampsjaak <rotor3d@gmail.com>
2021-04-16 23:09:23 +0200

    soeper console quads

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include "gl/gl.h"
 #include "GLFW/glfw3.h"
+#include <cmath>
 
 void processInput(GLFWwindow* window)
 {
@@ -14,9 +15,29 @@ void processInput(GLFWwindow* window)
 	}
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 	{
-		
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+	}
+}
+
+void AAQuad(float topX, float topY, float sizeX, float sizeY)
+{
+	glBegin(GL_TRIANGLE_STRIP);
+	glColor3f(0.0, 1.0, 0.0);
+
+	glVertex3f(topX, topY, 0);
+	glVertex3f(topX + sizeX, topY, 0);
+	glVertex3f(topX, topY - sizeY, 0);
+	glVertex3f(topX + sizeX, topY - sizeY, 0);
+	glEnd();
+}
+
+void render_loop()
+{
+	for(char i = 0; i < 54; i++)
+	{
+		AAQuad(-0.9 + ((i % 18) * 0.1), 0.5 - (floor(i / 18) * 0.1), 0.08, 0.07);
 	}
 }
 
@@ -25,7 +46,14 @@ void main()
 	glfwInit();
 	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
 
+	glfwMakeContextCurrent(window);
+
 	glViewport(0, 0, 640, 480);
+
+	// line drawing stuff
+	//glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	//glOrtho(0.0, 640.0, 0.0, 480.0, 0.0, 1.0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -33,7 +61,7 @@ void main()
 		processInput(window);
 
 		// rendering
-
+		render_loop();
 
 		// confusing bits
 		glfwPollEvents();
